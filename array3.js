214d28246b10632e19f6a4ab744bcb26c104f0d0
Ronald Espitia <ronald79@gmail.com>
2025-05-02 21:32:30 -0500

    Clase Mayo 2

---
+++ array3.js
@@ -0,0 +1,23 @@
+// Desempaquetamiento o desestructuracion de un array
+
+const num = [100, 11, 21];
+const [k, sol, num1, belony] = num;
+
+console.log(k);
+console.log(sol);
+console.log(num1);
+console.log(belony);
+console.log(num);
+
+// Matrices - Array Multidimensionales
+
+const letras = [['a', 'e', 'u'], ['z', 'b', 'm']]; // bidimensional
+//console.log(letras[1][1]);
+
+const letras2 = [['a', ['e', 'u']], [['b', 'r']]];
+
+//console.log(letras2[1][0][0]);
+//console.log(letras2[0][0]);
+console.log(letras2[0][1][1]);
+
+// 
---
+++ literal_templates.js
@@ -0,0 +1,11 @@
+// literal templates
+// interpolacion de variables o incluso podemos incrustar expresiones
+//console.log(`Hola mundo`);
+
+const a = 'Hola';
+const y = 'Belony';
+console.log(`Esto es una prueba de LT con este mensaje: ${a} ${y}`);
+
+const x = 2;
+const z = 3;
+console.log(`El resultado de sumar ${x} y ${z} es => ${x + y}`);
