e1c266bd0d13d4cbb29149edce999fbbdb45cab8
Ronald Espitia <ronald79@gmail.com>
2025-07-15 22:05:49 -0500

    clase Julio 15

--- fs.js
+++ fs.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-//const archivo = 'Esto es una prueba de guardar archivo con writefile y CB';
+//const archivo = 'Esto es una prueba de guarda archivo con writefile y CB';
 
 const carro = {
     marca: 'Toyota',
@@ -10,6 +10,48 @@ const carro = {
 
 const jsonCarro = JSON.stringify(carro);
 
-fs.writeFile('./prueba.json', jsonCarro, () => console.log('Se guardo correctamente'));
+// Escribir archivos Asincrono
 
-console.log('ESTE ES UN CL DESPUES DEL WRITEFILE');
+//fs.writeFile('./prueba.json', jsonCarro, 'utf-8', () => console.log('Se guardo correctamente'));
+
+//console.log('ESTE ES UN CL DESPUES DEL WRITEFILE');
+
+
+/* fs.writeFile('./xx/prueba2.json', jsonCarro, 'utf-8', err => {
+         if (err) {
+             console.log(`ocurrio un error de codigo: ${err.errno}`);
+        }
+        else {
+            console.log('Archivo guardado correctamente');
+        } 
+
+         console.log(err ? `ocurrio un error de codigo: ${err.errno}` : 'Archivo guardado correctamente'); 
+    if (err) {
+        console.log(`ocurrio un error de codigo: ${err.errno}`);
+        return;
+    }
+    console.log('Archivo guardado correctamente');
+});
+*/
+
+// Escribir archivos Sincronico
+
+const sistemaSolar = [
+    { planeta: 'Mercurio', composicion: 'Rocoso', ubicacion: '1' },
+    { planeta: 'Saturno', composicion: 'Gaseoso', ubicacion: '6' },
+    { planeta: 'Marte', composicion: 'Rocoso', ubicacion: '4' },
+    { planeta: 'Venus', composicion: 'Rocoso', ubicacion: '2' },
+];
+
+const sistemaJS = JSON.stringify(sistemaSolar);
+
+console.log('iniciando');
+
+try {
+    fs.writeFileSync('./planetas.json', sistemaJS, 'utf-8');
+    console.log('Archivo guardado correctamente');
+} catch (error) {
+    console.log('se produjo un error al guardar el archivo');
+}
+
+console.log('este es el final del codigo');
