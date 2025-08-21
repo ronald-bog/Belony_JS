d0ac6d58e9cb90c4c6d3a38510c4f1127dca8291
Ronald Espitia <ronald79@gmail.com>
2025-08-20 22:15:47 -0500

    Clase agosto 20

--- sql/index.js
+++ sql/index.js
@@ -2,6 +2,7 @@ const mysql = require('mysql2/promise');
 const express = require('express');
 const app = express();
 const PORT = 3001;
+app.use(express.json());
 
 const infoBase = {
     host: 'localhost',
@@ -12,12 +13,44 @@ const infoBase = {
 
 const pool = mysql.createPool(infoBase);
 
-app.get('/test', async (req, res) => {
-    const sql = 'SELECT * FROM usuarios';
-    const [rows] = await pool.execute(sql);
-    console.log(rows);
+app.get('/usuarios', async (req, res) => {
+    try {
+        const sql = 'SELECT * FROM usuarios';
+        const [rows] = await pool.execute(sql);
+
+        if (rows.length === 0) {
+            return res.status(202).json({ message: "La base de datos esta vacia" });
+        }
+
+        const respuesta = {
+            message: 'Listado de usuarios',
+            result_sql: 'OK',
+            data: rows
+        };
+        res.status(200).json(respuesta);
+    } catch (error) {
+        res.status(500).json({ message: 'Ocurrio un error en base de datos', success: false, errorQ: error });
+    }
+});
+
+app.get('/usuarios/:id', async (req, res) => {
+    const idx = req.params.id;
+    //const sql = 'SELECT * FROM usuarios WHERE id =' + idx; // no recomendable para evitar inyeccion sql
+    //const sql = 'SELECT * FROM usuarios WHERE id = ?' // (?) parametro posicional, placeholder, especificador
+    const [result] = await pool.execute('SELECT * FROM usuarios WHERE id = ?', [idx]);
+    console.log(result[0]);
 });
 
 app.listen(PORT, () => {
     console.log('Servidor ok');
+});
+
+app.post('/usuarios', async (req, res) => {
+    const { nombre, correo } = req.body;
+    //const nombre = body.nombre
+    //const correo = body.correo
+
+    const sql = 'INSERT INTO usuarios (nombre, correo) VALUES (?,?)';
+    const respuesta = await pool.execute(sql, [nombre, correo]);
+    res.status(201).json(respuesta);
 });
