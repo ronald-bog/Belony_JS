1487698eebe9e095b62b2a9db90260528daab8a8
Ronald Espitia <ronald79@gmail.com>
2025-09-23 22:10:49 -0500

    Clase sep 23

---
+++ POO/01-objeto.js
@@ -0,0 +1,11 @@
+// OBJETOS
+
+const persona = {
+    nombre: 'Hulk',
+    edad: 30,
+    saludar() {
+        console.log('Hola estoy aprendiendo POO');
+    }
+};
+
+console.log(persona.saludar());
