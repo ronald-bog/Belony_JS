8058d75afbd1ebbdfb2e92fd1db1596dc1569eb0
Ronald Espitia <ronald79@gmail.com>
2025-10-07 18:22:51 -0500

    Clase Octubre 6

---
+++ POO/07-abstraccion.js
@@ -0,0 +1,4 @@
+//* Proceso de simplificar sistemas complejos mediante la creación de modelos que ocultan los detalles internos y exponen solo lo necesario para el usuario final.
+
+//* Esta estrechamente relacionado con el encapsulamiento
+
