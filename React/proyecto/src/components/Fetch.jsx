eb21e8e84912b968aebb7bf134cbfb865a0ce1af
Ronald Espitia <ronald79@gmail.com>
2025-12-01 22:09:28 -0500

    Class dec 1

--- React/proyecto/src/components/Fetch.jsx
+++ React/proyecto/src/components/Fetch.jsx
@@ -2,33 +2,78 @@ import { useState } from "react";
 
 export function Fetch() {
     const [usuarios, setUsuarios] = useState([]);
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [mal, setMal] = useState(null);
 
     async function loadUsers() {
+        setLoading(true);
         try {
             const resultado = await fetch("https://jsonplaceholder.typicode.com/users");
-            console.log(resultado);
 
-            if(resultado){
-                console.log('No se pudo cargar la base de datos');
-             
+            //if (resultado.ok === false) 
+            if (!resultado.ok) {
+                throw new Error('Error al cargar usuarios'); //generamos excepcion manual
             }
 
             const datosBody = await resultado.json();
             setUsuarios(datosBody);
         } catch (error) {
+            console.log(error.message);
+            setMal(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+
+    }
+
+    async function enviarUsuario(e) {
+        e.preventDefault(); // evitar recarga por defecto de hmtl al usar formularios
+
+        try {
+            const respuesta = await fetch("https://jsonplaceholder.typicode.com/user", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ nombre: nombre, apellido: apellido })
+            });
+
+            if (!respuesta.ok) {
+                throw new Error('Error al guardar usuario');
+            }
+
+            const datosRta = await respuesta.json();
+            console.log(datosRta);
+
+        } catch (error) {
             console.log(error);
+
         }
     }
 
     return (
         <>
             <h1>*** Clase de fetch ***</h1>
+            {loading && <h3 className="loading">Cargando.....</h3>}
+            <h6>{mal}</h6>
             <button onClick={loadUsers}>LOAD</button>
             <ul>
                 {usuarios.map(obj =>
                     <li key={obj.id}>Username: {obj.username} - {obj.name} - {obj.id}</li>)
                 }
             </ul>
+            <br />
+            <h1>____ Ejemplo fetch con post ___</h1>
+            <br />
+
+            <form onSubmit={enviarUsuario}>
+                <input type="text" placeholder="Nombre" onChange={event => setNombre(event.target.value)} />
+
+                <input type="text" placeholder="Apellido" onChange={event => setApellido(event.target.value)} />
+
+                <button type="submit">Guardar Usuario</button>
+            </form>
         </>
     );
 }
