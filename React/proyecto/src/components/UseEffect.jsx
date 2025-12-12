25cd951d895357c65b1bd5a79be2fe1e57e70793
Ronald Espitia <ronald79@gmail.com>
2025-12-11 22:03:06 -0500

    Class dec 11

--- React/proyecto/src/components/UseEffect.jsx
+++ React/proyecto/src/components/UseEffect.jsx
@@ -1,16 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function UseEffect() {
 
+    const [update, setUpdate] = useState(false);
+    const [numero, setNumero] = useState(0);
+
     useEffect(() => {
         console.log('Se ejecuto dentro useEffect');
-    }, []);
+    }, [numero]);
 
     console.log('Se ejecuto fuera useEffect , se renderizo componente');
 
+    function actualiza() {
+        if (update === false) {
+            setUpdate(true);
+        } else {
+            setUpdate(false);
+        }
+        console.log(update);
+        /*         setUpdate(update === false ? true : false);
+                console.log(update); */
+    }
+
+    function cambiarNumero() {
+        setNumero(numero + 1);
+        console.log(numero);
+
+    }
+
     return (
         <>
-            UseEffect...........
+            <button onClick={actualiza}>ACTUALIZAR</button>
+
+            <button onClick={cambiarNumero}>INCREMENTAR NUMERO</button>
         </>
     );
 }
